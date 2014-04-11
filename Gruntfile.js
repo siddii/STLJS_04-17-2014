5824be6276a75c188146be69f642cf0b260b286b
Siddique Hameed <siddii+git@gmail.com>
2014-04-11 11:23:07 +0530

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,28 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
 
+  function csp(){
+    return function(req, res, next){
+      res.setHeader("X-WebKit-CSP", "default-src 'self' http://www.html5rocks.com/");
+      res.setHeader("X-Content-Security-Policy", "default-src 'self' http://www.html5rocks.com/");
+      res.setHeader("Content-Security-Policy", "default-src 'self' http://www.html5rocks.com/");
+      next();
+    };
+  }
+
   grunt.initConfig({
     connect: {
       server: {
         options: {
-          keepalive: true,
-          middleware: function (connect, options, middlewares) {
-            middlewares.push(function (req, res, next) {
-              if (req.url !== '/hello/world') return next();
-              res.end('Hello, world from port #' + options.port + '!');
-            });
-            return middlewares;
-          }
+          base: '.',
+          keepalive: true
+//          middleware: function(connect, options){
+//            return [
+//              csp(),
+//              connect.static(String('.')),
+//              connect.directory(String('.'))
+//            ];
+//          }
         }
       }
     }
